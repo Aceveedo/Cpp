4ef5f10a41d73cef65101acda849aa398e6dc0f5
Aceveedo <alvaro.acevedo@itgranada.com>
2022-01-25 10:31:35 +0100

    Realizacion de un bucle for para 10 juegos

--- dia3.cpp
+++ dia3.cpp
@@ -1,9 +1,28 @@
 #include <iostream>
 using namespace std;
 
+//void mostrarArray (string array[]){
+  //for(int i = 0 ; i < array->size() ; i++)
+  //cout << i << " = " << array[i] << endl;
+  //}
+//}
+
 int main(){
+int  numJuegos = 10; //Declaramos funciones para nombrarlas en el bucel for, este bucle basicamente lo que hace es nombrar 10 juegos y cada juego se llame "A"
+string juegos[numJuegos];
 
+  for (int i = 0; i < numJuegos ; i++){ //Empieza el bucle for
+    string jue;
+    cout << "Introduce el nombre de un juego: ";
+    cin >> jue;
+    juegos[i] = jue;
+  }
 
+  for (int i = 0; i < numJuegos ; i++){
+    cout << "juegos" << i << " = " << juegos[i] << endl;
+  } //Termina el bluce for
+
+  //mostrarArray(juegos);
 
 return 0;
 }
