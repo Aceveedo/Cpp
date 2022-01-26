8eb84487dd68e00c28d27e53d1a200184606cf79
Aceveedo <alvaro.acevedo@itgranada.com>
2022-01-26 13:16:40 +0100

    Correcion de algunos errores

--- Examen.cpp
+++ Examen.cpp
@@ -6,23 +6,28 @@ int aniosADias (int anios){
   return DIASENANIO*anios;
 }
 
-int aniosAMeses (int meses){
-  const int MESESENANIO = 365;
+int aniosAMeses (int anios){
+  const int MESESENANIO = 12;
   return MESESENANIO*anios;
 }
 
 int dientesCaidos(int anios){
   int meses;
-  int total;
+  int total = 0;
 
   meses = aniosAMeses(anios);
-  for(int i = o ; i <= meses ; i += 5){
+  for(int i = 0 ; i <= meses ; i += 5){
   total += i;
   }
+  return total;
 
 }
 
 int main(){
+  int mesesTiburon;
+  int diasTiburon;
+  int aniosTiburon;
+  int totalDientes;
 
 // 1.- Imprime tu nombre por pantalla.
 
@@ -30,25 +35,26 @@ cout << "Alvaro Acevedo Moreno" << endl;
 
 // 2.- ¿Cuantos años tiene el tiburon?
 
-int aniosTiburon;
+
 cout <<"¿Cuantos anios tiene el tiburon?";
 cin >> aniosTiburon;
 cout << "El tiburon tiene " << aniosTiburon << " anios" << endl;
 
 // 3.- Partiendo de un numero de años, calcula cuanto tiempo en dias y cuánto tiempo en meses ha pasado
-int mesesTiburon;
-int diasTiburon;
+
 
 diasTiburon = aniosADias(aniosTiburon);
 cout << "El tiburon itene " << diasTiburon << " dias" << endl;
 
 mesesTiburon = aniosAMeses(aniosTiburon);
-cout << "El tiburon itene " << mesesTiburon << " dias" << endl;
+cout << "El tiburon itene " << mesesTiburon << " meses" << endl;
 
 // 4.- Cada cinco meses al tiburon se le caen tantos dientes como meses tenga, es decir: A los 5 meses se le caen 5 dientes, a los 10 meses se le caen 10 dientes, En total se le han caido 15, A los 15 meses se le caen 15 dientes. En total se le han caido 30.
 // Calcula  cuantos dientes se le ha ncaido al tiburon.
 
-totalDientes = dientesCaidos(aniosTiburon)
+totalDientes = dientesCaidos(aniosTiburon);
+cout << "Al tiburon se le han caido " << totalDientes << " dientes" << endl;
+
 
 return 0;
 }
