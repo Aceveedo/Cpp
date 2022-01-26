2fd9506f9034ad0522105e039da3fa5519aa3e19
Aceveedo <alvaro.acevedo@itgranada.com>
2022-01-26 12:55:32 +0100

    Examen parte 2

--- Examen.cpp
+++ Examen.cpp
@@ -6,6 +6,22 @@ int aniosADias (int anios){
   return DIASENANIO*anios;
 }
 
+int aniosAMeses (int meses){
+  const int MESESENANIO = 365;
+  return MESESENANIO*anios;
+}
+
+int dientesCaidos(int anios){
+  int meses;
+  int total;
+
+  meses = aniosAMeses(anios);
+  for(int i = o ; i <= meses ; i += 5){
+  total += i;
+  }
+
+}
+
 int main(){
 
 // 1.- Imprime tu nombre por pantalla.
@@ -25,6 +41,14 @@ int diasTiburon;
 
 diasTiburon = aniosADias(aniosTiburon);
 cout << "El tiburon itene " << diasTiburon << " dias" << endl;
+
+mesesTiburon = aniosAMeses(aniosTiburon);
+cout << "El tiburon itene " << mesesTiburon << " dias" << endl;
+
+// 4.- Cada cinco meses al tiburon se le caen tantos dientes como meses tenga, es decir: A los 5 meses se le caen 5 dientes, a los 10 meses se le caen 10 dientes, En total se le han caido 15, A los 15 meses se le caen 15 dientes. En total se le han caido 30.
+// Calcula  cuantos dientes se le ha ncaido al tiburon.
+
+totalDientes = dientesCaidos(aniosTiburon)
 
 return 0;
 }
