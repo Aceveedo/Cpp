badc36fbe07f8388133d57c82d42dbd3638e5f47
Aceveedo <alvaro.acevedo@itgranada.com>
2022-01-27 13:48:40 +0100

    Arrays y void dia3

--- dia3.cpp
+++ dia3.cpp
@@ -1,3 +1,4 @@
+
 #include <iostream>
 using namespace std;
 
@@ -7,9 +8,35 @@ using namespace std;
   //}
 //}
 
+int multiplicax5 (int num){
+  return num*5;
+}
+
+void meMultiplicox5 (int &num){
+  return num*5;
+  cout << "Num en la funcion vale: " << num << endl;
+}
+
+void subirNota (int notas[3], int posicion, int nuevaNota){
+  notas[posicion] = nuevaNota;
+}
+
+void imprimirArray(int array[20], int tama){
+  for (int i = 0 ; i < tama ; i++){
+      cout << array[i] << endl;
+  }
+}
+
 int main(){
-int  numJuegos = 10; //Declaramos funciones para nombrarlas en el bucel for, este bucle basicamente lo que hace es nombrar 10 juegos y cada juego se llame "A"
+int  numJuegos = 100; //Declaramos funciones para nombrarlas en el bucel for, este bucle basicamente lo que hace es nombrar 10 juegos y cada juego se llame "A"
 string juegos[numJuegos];
+int numero1 = 2;
+int numero2 = 0;
+int notas[] = {8,4,9};
+
+
+/*cout << "¿Cuantos juegos quieres introducir? ";
+cin >> numJuegos;
 
   for (int i = 0; i < numJuegos ; i++){ //Empieza el bucle for
     string jue;
@@ -19,10 +46,22 @@ string juegos[numJuegos];
   }
 
   for (int i = 0; i < numJuegos ; i++){
-    cout << "juegos" << i << " = " << juegos[i] << endl;
+  cout << "juegos" << i << " = " << juegos[i] << endl;*/
   } //Termina el bluce for
 
-  //mostrarArray(juegos);
+numero2 = multiplicax5(numero1);
+cout << "El numero1 es " << numero1 << endl;
+cout << "El numero2 es " << numero2 << endl;
+
+meMultiplicox5(numero1);
+cout << "El numero1 es " << numero1 <<endl << endl;
+
+cout <<"Notas original" << endl:
+imprimirArray(notas, 3);
+cout << "Subimos la nota" << endl;
+subirNota(notas,1,9);
+cout << "Nota tras subir la nota " << endl;
+imprimirArray(notas,3);
 
 return 0;
 }
