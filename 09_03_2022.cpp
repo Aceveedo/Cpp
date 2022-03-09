a36e3e364638e0ee918e59bef64689507bc57859
Aceveedo <alvaro.acevedo@itgranada.com>
2022-03-09 12:02:06 +0100

    Empezando la clase de poligonos de programacion de Celia

---
+++ 09_03_2022.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <math.h>
+#include <vector>
+using namespace std;
+
+class  Poligono{
+  private:
+    vector <double> lados;
+  public:
+    Poligono(vectpor <double> lados){
+      this ->lados = lados;
+    }
+};
+
+int main(){
+
+  vector <double> ladoTriangulo {10,15,20};
+
+  cout << ladosTriangulo[0] << endl;
+
+  return 0;
+}
